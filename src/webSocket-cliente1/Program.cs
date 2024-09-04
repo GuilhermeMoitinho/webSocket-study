92c3b8fcf3735e17a51ee32943f8ea815db004a7
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2024-09-04 19:27:06 -0300

    organization client

--- src/webSocket-cliente1/Program.cs
+++ src/webSocket-cliente1/Program.cs
@@ -7,35 +7,33 @@ class Program
     static async Task Main(string[] args)
     {
         var uri = new Uri("ws://localhost:8080/");
-        using (var client = new ClientWebSocket())
+        using var client = new ClientWebSocket();
+        await client.ConnectAsync(uri, CancellationToken.None);
+        Console.WriteLine("Conectado ao servidor WebSocket.");
+
+        // Enviar o nome do cliente
+        Console.Write("Digite seu nome: ");
+        var name = Console.ReadLine();
+        var nameBuffer = Encoding.UTF8.GetBytes($"NAME:{name}");
+        var nameSegment = new ArraySegment<byte>(nameBuffer);
+        await client.SendAsync(nameSegment, WebSocketMessageType.Text, true, CancellationToken.None);
+
+        var receiveTask = ReceiveMessage.ReceiveMessagesAsync(client);
+
+        while (true)
         {
-            await client.ConnectAsync(uri, CancellationToken.None);
-            Console.WriteLine("Conectado ao servidor WebSocket.");
+            Console.Write($"{name}> ");
+            var sendMessage = Console.ReadLine();
+            if (sendMessage == "exit")
+                break;
 
-            // Enviar o nome do cliente
-            Console.Write("Digite seu nome: ");
-            var name = Console.ReadLine();
-            var nameBuffer = Encoding.UTF8.GetBytes($"NAME:{name}");
-            var nameSegment = new ArraySegment<byte>(nameBuffer);
-            await client.SendAsync(nameSegment, WebSocketMessageType.Text, true, CancellationToken.None);
+            var sendBuffer = Encoding.UTF8.GetBytes(sendMessage);
+            var sendSegment = new ArraySegment<byte>(sendBuffer);
 
-            var receiveTask = ReceiveMessage.ReceiveMessagesAsync(client);
-
-            while (true)
-            {
-                Console.Write($"{name}> ");
-                var sendMessage = Console.ReadLine();
-                if (sendMessage == "exit")
-                    break;
-
-                var sendBuffer = Encoding.UTF8.GetBytes(sendMessage);
-                var sendSegment = new ArraySegment<byte>(sendBuffer);
-
-                await client.SendAsync(sendSegment, WebSocketMessageType.Text, true, CancellationToken.None);
-            }
-
-            await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Fechamento normal", CancellationToken.None);
-            Console.WriteLine("Conexão WebSocket fechada.");
+            await client.SendAsync(sendSegment, WebSocketMessageType.Text, true, CancellationToken.None);
         }
+
+        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Fechamento normal", CancellationToken.None);
+        Console.WriteLine("Conexão WebSocket fechada.");
     }
 }
